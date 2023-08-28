c0b7b259ec3009cd61f227d461d472a7a21da322
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-08-28 17:29:07 +0900

    2023.8.28 sfml 적용

---
+++ KjwApplication1/소스.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Graphics.hpp>
+
+#include <time.h>
+#include <iostream>
+
+#define COL 16
+#define ROW 16
+#define BOMB_CNT 20
+
+#define TILE_PIXCEL 32
+
+#define BLANK 0
+#define BOMB 9
+#define BOOM 10
+#define UNBLANK 11
+#define FLAG 12
+#define DONTKNOW 13 
+
+using namespace sf;
+using namespace std;
+
+
+
+int main()
+{
+
+    return 0;
+}
